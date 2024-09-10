d1c844c3a35d91f46217e0c171d6dab9e0027bd2
Coosis <1159727122@qq.com>
2024-09-10 11:26:09 +0800

    fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 		pkgs = import nixpkgs { system = "aarch64-darwin"; };
 	in
 	{
-		devShells.aarch64-darwin = pkgs.mkShell {
+		devShells.aarch64-darwin.default = pkgs.mkShell {
 			packages = [
 				(pkgs.python3.withPackages (p: with p; [
 					flask
